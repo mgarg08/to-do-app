8e6c3058e76108cf428369b49af99978b2d1d542
Mudit Garg <muditeduca@gmail.com>
2024-04-06 11:48:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const todos = [
+  {
+    id: 1,
+    title: "Buy groceries",
+    description: "Milk, eggs, bread, fruits",
+    due_date: 10 - 4 - 24,
+    priority: "High",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Finish report",
+    description: "Complete the quarterly report for the team",
+    due_date: "2024-04-15",
+    priority: "Medium",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Call mom",
+    description: "Wish her a happy birthday",
+    due_date: "2024-04-05",
+    priority: "Low",
+    completed: true,
+  },
+];
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList />
     </div>
   );
 }
 
-export default App;
+function TodoList() {
+  return (
+    <div className="container">
+      <h2>My Todo</h2>
+      <SearchBar />
+      <TodoItem items={todos} />
+    </div>
+  );
+}
+
+function SearchBar() {
+  return (
+    <>
+      <form className="search">
+        <input type="text" placeholder="add item..." />
+        <button>Add</button>
+      </form>
+    </>
+  );
+}
+
+function TodoItem({ items }) {
+  return (
+    <div className="list-Items">
+      {items.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <div className="items">
+      <div>{item.title}</div>
+      <button>❌</button>
+    </div>
+  );
+}
